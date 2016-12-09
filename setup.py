22dadde2f49fc66f4d237d02236681e038c07c65
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 08:56:37 -0800

    created setup file for new repo

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Setup for the code kata collection."""
+
+from setuptools import setup
+
+
+setup(
+    name="code-katas",
+    description="A work through of Python Katas",
+    version=0.1,
+    author="Claire Gatenby",
+    author_email="clairejgatenby@gmail.com",
+    license="MIT",
+    package_dir={'': 'src'},
+    py_modules=[""],
+    install_requires=['numpy'],
+    extras_require={
+        "test": ["pytest", "pytest-watch", "pytest-cov", "tox"]
+    },
+    entry_points={
+        "console_scripts": [
+            "ackermann = ackermann:print_something"]
+    }
+)
