1b03b5d83701940d8f72c8a98f1a5a0ff956cb3f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 12:50:42 -0800

    solved needle kata

---
+++ src/needle.py
@@ -0,0 +1,6 @@
+"""Return the word needle from haystack."""
+
+
+def find_needle(haystack):
+    """Return needle in haystack."""
+    return 'found the needle at position {}'.format(haystack.index('needle'))
