375a46757bb88b3a64264d4e618c816f4cdba849
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 09:22:18 -0800

    added codewars tests

---
+++ tests/test_even-or-odd.py
@@ -0,0 +1,7 @@
+"""Test even or odd function."""
+
+
+test.expect(even_or_odd(2) == "Even")
+test.expect(even_or_odd(0) == "Even")
+test.expect(even_or_odd(7) == "Odd")
+test.expect(even_or_odd(1) == "Odd")
