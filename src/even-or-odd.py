64c52a2d089de067bc489b0a6910429dccc4fd5a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 10:29:22 -0800

    renamed file

--- src/even-or-odd.py
+++
@@ -1,8 +0,0 @@
-"""Create a function to take a number and return odd or even."""
-
-def even_or_odd(n):
-    """Return a string odd or even for odd or even values of n."""
-    if n%2 == 0:
-        return 'Even'
-    else: 
-        return 'Odd'
