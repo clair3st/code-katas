3020b07d09a661f41323ec9b60af4c044e65b9fd
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 10:02:33 -0800

    moved file

--- tests/test_even-or-odd.py
+++
@@ -1,7 +0,0 @@
-"""Test even or odd function."""
-
-
-test.expect(even_or_odd(2) == "Even")
-test.expect(even_or_odd(0) == "Even")
-test.expect(even_or_odd(7) == "Odd")
-test.expect(even_or_odd(1) == "Odd")
