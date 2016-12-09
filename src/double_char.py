0264469e00a0ab81d927467889fe8a651f6b00cc
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-09 12:04:41 -0800

    created double char function

---
+++ src/double_char.py
@@ -0,0 +1,9 @@
+"""Function to return a string with each character repeated."""
+
+
+def double_char(s):
+    """Return word with double characters."""
+    word = ''
+    for i in s:
+        word += i * 2
+    return word
