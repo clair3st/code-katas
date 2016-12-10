8dded718c2a2ef6c9f5254f2c7b97c195d5660a6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-10 08:52:11 -0800

    added a case for n = 0

--- src/sum_series.py
+++ src/sum_series.py
@@ -3,6 +3,8 @@
 
 def sum_series(n):
     """Sum of the first nth value of a series."""
+    if n == 0:
+        return '0.00'
     a = sum([1.0 / (3 * i + 1) for i in range(n)])
     return '{number:.{digits}f}'.format(number=a, digits=2)
 
