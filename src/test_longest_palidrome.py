00af0b5420e9a3bc86f794a6001c8d56e48572a2
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-29 20:42:18 -0700

    tests for longest palindrome.

---
+++ src/test_longest_palidrome.py
@@ -0,0 +1,20 @@
+"""Test longest palindrome."""
+
+import pytest
+
+
+STRING = [
+    ("a", 1),
+    ("aa", 2),
+    ("baa", 2),
+    ("aab", 2),
+    ("abcdefghba", 1),
+    ("baablkj12345432133d", 9),
+]
+
+
+@pytest.mark.parametrize("n, result", STRING)
+def test_longest_palidrome(n, result):
+    """Test longest palindrome is found."""
+    from longest_palindrome import longest_palindrome
+    assert longest_palindrome(n) == result
