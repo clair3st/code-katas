df6eab3f12d35c5828d047a1ff0821454a39f7d2
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-16 12:32:02 -0700

    completed but very long

--- src/sum_pairs.py
+++ src/sum_pairs.py
@@ -5,8 +5,29 @@ def sum_pairs(ints, s):
     """Pair to sum up to s."""
     lookup = {}
     for n, i in enumerate(ints):
-        lookup.setdefault(i, n)
-    options = [[k, (s - k)] for k in lookup if (s - k) in lookup]
+        lookup.setdefault(i, []).append(n)
+
+    options = []
+
+    for k in lookup:
+        if s - k in lookup:
+            if s - k == k and len(lookup[s - k]) < 2:
+                continue
+            if s - k == k and len(lookup[s - k]) >= 2:
+                options.append(
+                    [k,
+                     s - k,
+                     max(lookup[k])]
+                )
+                continue
+            options.append(
+                [k,
+                 s - k,
+                 max(min(lookup[k]), min(lookup[s - k]))]
+            )
+    print(options)
+
     if options:
-        return min(options, key=lambda x: lookup[x[0]])
+        return sorted(min(options, key=lambda x: x[2])[:-1],
+                      key=lookup.__getitem__)
     return None
