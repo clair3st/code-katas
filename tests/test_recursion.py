5d15874860be07f46a10f7a7c36bbf9412bac4a3
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-22 17:00:55 -0700

    added tests for fib

--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -9,9 +9,23 @@ PARAMS = [
     (0, 0)
 ]
 
+FIB_PARAMS = [
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (7, 13)
+]
+
 
 @pytest.mark.parametrize('n, result', PARAMS)
 def test_sum_of_n(n, result):
     """Test factorial sumation."""
     from src.recursion_practise import sum_of_n
     assert sum_of_n(n) == result
+
+
+@pytest.mark.parametrize('n, result', FIB_PARAMS)
+def test_fib(n, result):
+    """Test factorial sumation."""
+    from src.recursion_practise import fibonacci
+    assert fibonacci(n) == result
